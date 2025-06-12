fda0aaada05c8969a4fe40bef15cdd6aa17d84b2
Duong <ltson@theodorealexander.com>
2025-06-12 10:58:47 +0700

    Responsive breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xsm: "500px",
+        sm: "600px",
+        md: "690px",
+        lg: "988px",
+        xl: "1078px",
+        xxl: "1265px"
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        textGray: "#71767b",
+        textGrayLight: "#e7e9ea",
+        borderGray: "#2f3336",
+        inputGray: "#20327",
+        iconBlue: "#1d9bf0",
+        iconGreen: "#00ba7c",
+        iconPink: "#f91880"
+      }
     },
   },
   plugins: [],
