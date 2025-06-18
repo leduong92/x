1c79467a3057d1adb4c322ee706a67c9c41deb7f
Duong <ltson@theodorealexander.com>
2025-06-18 13:25:53 +0700

    Post design

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -14,21 +14,22 @@ const Post = () => {
                 <div className='relative w-10 h-10 rounded-full overflow-hidden'>
                     <Image src={`/general/avatar.png`} alt='avatar' width={100} height={100} />
                 </div>
-                <div className=''>
-                    <div>
-                        <div className='flex justify-between'>
-                            <div className='flex items-center gap-2 flex-wrap'>
-                                <h1>Dev</h1>
-                                <span>@leduong92</span>
-                                <span>1 day ago</span>
-                            </div>
-                            <PostInfo />
+                <div className='flex-1 flex flex-col gap-2'>
+                    <div className='flex items-center justify-between gap-2'>
+                        <div className='flex items-center gap-2 flex-wrap'>
+                            <h1 className='text-md font-bold'>Duong Dev</h1>
+                            <span className='text-textGray'>@leduong92</span>
+                            <span className='text-textGray'>1 day ago</span>
                         </div>
+                        <PostInfo />
                     </div>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eos ratione dignissimos reiciendis commodi veniam harum consectetur assumenda vel odio voluptas numquam repellat rem dolorum, magnam excepturi non? Dolore, facilis.
+                    </p>
+                    <Image src={`/general/post.jpeg`} alt="" width={600} height={600} />
                 </div>
             </div>
-
-
         </div>
     )
 }
---
+++ src/components/PostInteractions.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+
+const PostInteractions = () => {
+    return (
+        <div className='flex items-center justify-center my-2 text-textGray'>
+            <div className=''>
+
+            </div>
+            <div className=''></div>
+        </div>
+    )
+}
+
+export default PostInteractions
